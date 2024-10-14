0882280e86f69147264ecfd338d8fb72f22cc06b
Falak Niyaz <118990119+falakniyaz94@users.noreply.github.com>
2024-10-14 15:17:07 +0000

    config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId = 454844785
+let accountEmail ="falak@gmail.com"
+var accountPass ="123"
+accountCity ="jalandhar"
+
+let accountState;
+
+console.log(accountCity);
+accountCity="Banglore"
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
+
+// accountId=44 not allowed
+accountEmail ="ncj@gma.com"
+accountPass = "hbwshb"
+
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
