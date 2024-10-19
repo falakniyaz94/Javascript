c9d75b01d06eb398d9c48d60f141ab8845bb0d48
Falak Niyaz <118990119+falakniyaz94@users.noreply.github.com>
2024-10-19 16:35:06 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer versions
+
+//alert(3+3)//we are using node js , not browser
+
+let name = "falak"
+let age = "20"
+let islogged = "false"
+
+// number=> 2 to power 53
+// BigInt
+// String => ""
+// Boolean => true/false
+// null // standalone value
+// undefined
+// symbol => unique
+
+
+//object
+
+console.log(typeof null) //object
+console.log(typeof undefined); //undefined
