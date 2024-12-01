874f7aa9bca42ddaecee2fb94141efa7f0ea1522
Falak Niyaz <118990119+falakniyaz94@users.noreply.github.com>
2024-12-01 07:57:42 +0000

    container cofig

---
+++ 01_basics/04_primitives_objects.js
@@ -0,0 +1,17 @@
+// nn ss bb u
+let a=null;
+let b=340;
+let c="Falak";
+let d=Symbol("this is symbol");
+let e=true;
+let f=BigInt("576")+BigInt("40");
+let g=undefined;
+console.log(a,b,c,d,e,f,g);
+
+//non primitive datatype->object
+const item =
+{
+    "Name":"Falak",
+    "Age":22
+};
+console.log(item["class"])
